aad9d281b998fbd221d0f79b8aba4616d68e7bbe
ccarn <ccarney651@gmail.com>
2026-08-08 01:39:55 +0100

    Remove retired native GUI files (gui.py, app entry, PyInstaller specs, launcher)

--- Scout app.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "APP=.venv\Scripts\owscout-app.exe"
-if not exist "%APP%" (
-  echo ERROR: %APP% not found. Install with:  .venv\Scripts\pip install -e .[capture]
-  pause & exit /b 1
-)
-start "" "%APP%"
